d8ca589047b1c997969a1942b263cf7d50fec97a
leo <work.mileo@gmail.com>
2020-12-04 09:20:30 +0800

    update

--- src/server/image.js
+++ src/server/image.js
@@ -1,5 +1,5 @@
 import imageMgr from '@xq/image'
-import configMgr from 'biz/config.js'
+import configMgr from '../biz/config.js'
 
 function setImageUrlKey(key) {
   configMgr.getDetail('xq_image_baseurl').then(res => {

096e8a70667b777ecff3176e99d0331689cc5f16
leo <work.mileo@gmail.com>
2020-12-31 17:09:17 +0800

    update

--- src/server/fetch.js
+++ src/server/fetch.js
@@ -15,7 +15,7 @@ let apis = {
     login: post('/v1/user/wxLogin'),
     auth: post('/v1/user/getWxUserInfo'),
     phone: post('/v1/user/getWxUserPhone'),
-    log: get('/v1/user/createLogLogin'),
+    log: post('/v1/user/createLogLogin'),
   },
   file: {
     upload: upload('/upload')

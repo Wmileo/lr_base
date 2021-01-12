ca9dd47cabc103c026e6ec6e59e361acf7617577
leo <work.mileo@gmail.com>
2021-01-12 17:43:53 +0800

    update

--- mp/utils/env.js
+++ mp/utils/env.js
@@ -19,13 +19,13 @@ function shake() {
     let subx = Math.abs(lx - res.x)
     let suby = Math.abs(ly - res.y)
     let subz = Math.abs(lz - res.z)
-    if (subx > 1.2 || suby > 1.2 || subz > 1.2) {
+    if (subx > 1 || suby > 1 || subz > 1) {
       time++
     }
     lx = res.x
     ly = res.y
     lz = res.z
-    if (time > 3) {
+    if (time > 2) {
       time = 0
       $api.showActionSheet(envs).then(res => {
         let env = envs[res.tapIndex]

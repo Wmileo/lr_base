9c20ce1c7ac24aad2b2975f884a0ed1496195e28
leo <work.mileo@gmail.com>
2021-03-26 17:07:40 +0800

    update

--- uni/biz/qrcode.js
+++ uni/biz/qrcode.js
@@ -1,11 +1,18 @@
 function mp(key, path) {
   let tmp = path.split('?')
-  let scene = tmp[1] ? tmp[1].replace(new RegExp(`\\&`, 'g'), ',') : " "
-  let page = tmp[0].substr(1)
+  let scene = "channel=qrcode"
+  if (tmp[1] && tmp[1].length > 0) {
+    scene += ',' + tmp[1].replace(new RegExp(`\\&`, 'g'), ',')
+  }
+  let page = tmp[0]
+  if (page.indexOf('/') == 0) {
+    page = page.substr(1)
+  }
+  console.log(scene)
   return $fetch.qrcode.mpqrcode().fetch({
     key,
     scene,
-    page: null,
+    page,
     // width: 430,
     // autoColor: false,
     // lineColor: "{\"r\":255,\"g\":165,\"b\":0}",
---
+++ uni/utils/canvas.js
@@ -0,0 +1,39 @@
+function draw(id, infos) {
+  let ctx = uni.createCanvasContext(id)
+  infos.forEach(item => {
+    if (item.type == 'image') {
+      drawImage(ctx, item)
+    }
+  })
+
+}
+
+function drawImage(ctx, data) {
+  uni.downloadFile({
+    url: data.url,
+    success(res) {
+      if (res.statusCode == 200) {
+        uni.getImageInfo({
+          src: res.tempFilePath,
+          success: function(image) {
+            let x = data.x || 0
+            let y = data.y || 0
+            let width = data.width || image.width
+            let height = data.height || image.height
+            ctx.drawImage(res.tempFilePath, x, y, width, height)
+            ctx.draw()
+          }
+        })
+      }
+    }
+  })
+}
+
+if (!$utils) {
+  $utils = {}
+}
+
+$utils.canvas = {
+  draw
+}
+

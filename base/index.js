3df7137d70a59247f2c21db501efe3d8d677a753
leo <work.mileo@gmail.com>
2021-03-29 20:01:22 +0800

    update

--- base/index.js
+++ base/index.js
@@ -2,10 +2,10 @@ import moment from 'moment'
 import 'moment/locale/zh-cn'
 moment.locale('zh-cn')
 
-import '@xq/log'
 import '@xq/server'
 
 import './utils/notification.js'
+import './utils/log.js'
 import './utils/storage.js'
 import image from './utils/image.js'
 import './utils/url.js'
---
+++ base/utils/log.js
@@ -0,0 +1,74 @@
+let list = []
+let onList = (list) => {}
+
+function print(type, mark, msg, data) {
+  console[type](`xq-${mark}：${msg}`)
+  let datas = null
+  if (data) {
+    datas = []
+    if (data.constructor === Object) {
+      for (let key in data) {
+        console[type](`xq-${mark}：${key}`, data[key])
+        datas.push(`${key} :${str(data[key])}`)
+      }
+    } else if (data.constructor === Array) {
+      data.forEach(item => {
+        console[type](`xq-${mark}：`, item)
+        datas.push(str(item))
+      })
+    } else {
+      console[type](`xq-${mark}：`, data)
+      datas.push(data)
+    }
+    console[type](`xq-${mark}：----------------`)
+  }
+  let time = $moment().format('YYYY-MM-DD hh:mm:ss');
+  list.unshift({
+    type,
+    msg,
+    datas,
+    mark,
+    time
+  })
+  onList(list)
+}
+
+function str(item) {
+  if (typeof(item) == 'object') {
+    return JSON.stringify(item, null, 4)
+  } else {
+    return item
+  }
+}
+
+function log(mark, msg, data) {
+  print('log', mark, msg, data)
+}
+
+function warn(mark, msg, data) {
+  print('warn', mark, msg, data)
+}
+
+function info(mark, msg, data) {
+  print('info', mark, msg, data)
+}
+
+function error(mark, msg, data) {
+  print('error', mark, msg, data)
+}
+
+function handleList(func) {
+  onList = func
+  onList(list)
+}
+
+$log = {
+  log,
+  warn,
+  info,
+  error
+}
+
+export default {
+  handleList
+}

5bb570edb308c202d46bf9d50bb14af6a2c34744
shanshan <18410717570@163.com>
2021-05-25 11:28:36 +0800

    update

--- uni/utils/canvas.js
+++ uni/utils/canvas.js
@@ -10,7 +10,11 @@ class Canvas {
     for(let item of infos){
       if (item.type == 'image') {
         await this.drawImage(item);
-      }
+      } else if (item.type == 'rect') {
+				await this.drawRect(item);
+			} else if (item.type == 'text') {
+				await this.drawText(item);
+			}
     }
     
     return new Promise((resolve,reject)=>{
@@ -45,8 +49,35 @@ class Canvas {
       });
     });
   }
+	
+	drawRect(data) {
+		let ctx = this.ctx
+		return new Promise((resolve,reject)=>{
+			let x = data.x || 0
+			let y = data.y || 0
+			let width = data.width || 0
+			let height = data.height || 0
+		  ctx.setFillStyle(data.fillStyle)
+		  ctx.fillRect(x, y, width, height)
+			resolve()
+		});
+	}
+	
+	drawText(data) {
+		let ctx = this.ctx
+		return new Promise((resolve,reject)=>{
+			let text = data.text || ''
+			let x = data.x || 0
+			let y = data.y || 0
+			let maxWidth = data.maxWidth || 0
+		  ctx.setFillStyle(data.fillStyle)
+		  ctx.setFontSize(data.fontSize)
+		  ctx.fillText(text, x, y, maxWidth)
+			resolve()
+		});
+	}
   
-  image(){
+  image() {
     return new Promise((resolve, reject) => {
       uni.canvasToTempFilePath({
         canvasId: this.id,

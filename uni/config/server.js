f0f7f564185b91856b0dce33cdb489260eea086e
leo <work.mileo@gmail.com>
2021-07-01 15:51:33 +0800

    update

--- uni/config/server.js
+++ uni/config/server.js
@@ -1,20 +1,23 @@
 import server from '@xq/server'
 let time = 0
 let wait = false
-server.http.onFail((code, msg) => {
+
+let fail = (code, msg) => {
   if (code > 1 && msg) {
     $api.showToast(msg)
   }
   if (msg) {
     $log.warn('base-mp-config', 'serverOnFail', msg)
   }
-})
-server.http.onError(err => {
+}
+
+let err = (err) => {
   if (err.message) {
     $log.warn('base-mp-config', 'serverOnError', err.message)
   }
-})
-server.http.onAuth(() => {
+}
+
+let auth = () => {
   time++
   if (time >= 20) {
     if (!wait) {
@@ -27,4 +30,16 @@ server.http.onAuth(() => {
     return Promise.reject(new Error('服务出错，请稍后使用'))
   }
   return $auth.autoLogin(true)
+}
+
+server.http.onFail({
+  xq: fail,
+  jz: fail
+})
+server.http.onError({
+  xq: err,
+  jz: err
+})
+server.http.onAuth({
+  xq: auth
 })

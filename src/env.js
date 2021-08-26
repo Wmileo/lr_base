95dce33718e2816646b5fa63a74f6e1def40d619
leo <work.mileo@gmail.com>
2021-08-26 14:17:34 +0800

    update

--- src/env.js
+++ src/env.js
@@ -1,14 +1,18 @@
 const isUni = typeof(uni) != 'undefined'
-const system = isUni ? uni.getSystemInfoSync() || {} : {}
-const safeBottom = (isUni && (system.model.indexOf('iPhone X') >= 0 || system.model.indexOf('iPhone 1') >= 0)) ? 34 : 0
 
 let env = {
-  isH5: process.env.VUE_APP_PLATFORM == 'h5',
   isUni,
-  isH5InMp: window.__wxjs_environment === 'miniprogram',
-  isQy: system.environment == 'wxwork',
-  safeBottom,
-  isAndroid: system.system.indexOf('Android') == 0
+  isH5: true,
+  isH5InMp: window.__wxjs_environment === 'miniprogram'
+}
+
+if (isUni) {
+  const system = uni.getSystemInfoSync()
+  const safeBottom = (system.model.indexOf('iPhone X') >= 0 || system.model.indexOf('iPhone 1') >= 0) ? 34 : 0
+  env.isH5 = process.env.VUE_APP_PLATFORM == 'h5'
+  env.isQy = system.environment == 'wxwork'
+  env.safeBottom = safeBottom
+  env.isAndroid = system.system.indexOf('Android') == 0
 }
 
 export default env

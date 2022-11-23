e8e16f8c3a8a433fdf29e0097d5ee8d6fd5f0790
leo <work.mileo@gmail.com>
2022-11-23 15:53:43 +0800

    h5inwx

--- src/base/env.js
+++ src/base/env.js
@@ -2,12 +2,13 @@ const isUni = typeof(uni) != 'undefined'
 
 let env = {
   isUni,
-  isH5: true,
+  isH5: true
 }
 
 if (isUni) {
   const system = uni.getSystemInfoSync()
-  const safeBottom = (system.model.indexOf('iPhone X') >= 0 || system.model.indexOf('iPhone 1') >= 0) ? 34 : 0
+  const safeBottom = (system.model.indexOf('iPhone X') >= 0 || system.model.indexOf(
+    'iPhone 1') >= 0) ? 34 : 0
   env.isH5 = process.env.VUE_APP_PLATFORM == 'h5'
   env.isQy = system.environment == 'wxwork'
   env.safeBottom = safeBottom
@@ -16,7 +17,12 @@ if (isUni) {
 
 if (env.isH5) {
   env.isH5InMp = window.__wxjs_environment === 'miniprogram'
-  env.isH5InWx = !!window.WeixinJSBridge
+  env = {
+    ...env,
+    get isH5InWx() {
+      return !!window.WeixinJSBridge
+    }
+  }
 }
 
 export default env
